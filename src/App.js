70fe3ad066726f46cb22983d5a53eac09b1b2e67
ifetakuro <takurofisayo@gmail.com>
2022-06-02 14:44:26 +0100

    added darkmode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from "react"
+import React, {useState} from "react"
 import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 
 export default function App() {
+    const [mode, setMode] = useState(false)
+
+    function changeMode() {
+        setMode(prevMode => !prevMode)
+    } 
+
     return (
-        <div className="container">
-            <Navbar />
-            <Main />
+        <div>
+            <Navbar darkMode={mode} toggleMode={changeMode}/>
+            <Main darkMode={mode} />
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,18 @@
 import React from "react"
 import logo from '../images/logo192.png'
 
-export default function Navbar() {
+export default function Navbar(props) {
     return (
-      <nav>
+      <nav className={props.darkMode ? 'dark' : ''}>
         <img src={logo} alt="logo"/>
         <h3>ReactFacts</h3>
-        <h4>React Course - Project 1</h4>
+        <div className='toggler'>
+          <p className="toggle-light">Light</p>
+          <div onClick={props.toggleMode} className="toggle-slide" >
+            <div className="toggle-slide-circle"></div>
+          </div>
+          <p className="toggle-dark">Dark</p>
+        </div>
       </nav>
 
     )
